42180be04d991f44298dd1073422b2df65df161d
John Arocha <john.arocha@clover.com>
2025-02-11 12:08:40 -0700

    remoed outlines used for dev

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -11,17 +11,14 @@ const styles = {
     align-items: center;
     width: 100vw;
     height: 100vh;
-    outline: 1px solid red;
   `,
   pieChartContainer: css`
     width: 35vw;
     height: 35vw;
-    outline: 1px solid black;
   `,
   barChartContainer: css`
     width: 55vw;
     height: auto;
-    outline: 1px solid black;
   `,
 };
 
