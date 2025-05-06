5eccd5f245265ada8358bc20429653011a1c74d4
John Arocha <john.arocha@clover.com>
2025-05-06 10:57:32 -0600

    added psudocode

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,14 @@
 /** @jsxImportSource @emotion/react */
-import styles from './styles.ts';
-import NavBar from '../../components/Navbar/index.tsx';
-import PieChartComponent from '../../components/Charts/PiChart/index.tsx';
-import BarChart from '../../components/Charts/BarChart/index.tsx'
-import RadarChart from '../../components/Charts/RadarChart/index.tsx';
+import styles from "./styles.ts";
+import NavBar from "../../components/Navbar/index.tsx";
+import PieChartComponent from "../../components/Charts/PiChart/index.tsx";
+import BarChart from "../../components/Charts/BarChart/index.tsx";
+import RadarChart from "../../components/Charts/RadarChart/index.tsx";
 
 function Home() {
+  // add a useEffect that grabs the data and populates the charts here
+  // maybe add loaders for if thecharts dont have data just yet
+  
   return (
     <>
       <NavBar />
@@ -13,7 +16,9 @@ function Home() {
         <div css={styles.appContainer}>
           <div css={styles.pieChartContainer}>
             <p css={styles.pLeft}>
-              "Understanding your monthly expenses is the first step towards better financial management. Keep track of where your money goes to make informed decisions and achieve your financial goals."
+              "Understanding your monthly expenses is the first step towards
+              better financial management. Keep track of where your money goes
+              to make informed decisions and achieve your financial goals."
             </p>
             <PieChartComponent />
           </div>
@@ -22,15 +27,17 @@ function Home() {
           <div css={styles.barChartContainer}>
             <RadarChart />
             <p css={styles.pRight}>
-            <span style={{fontWeight: 'bold'}} >"Studies show that keeping your discretionary spending under 30% of your income can significantly boost your savings over time."</span> -Your friendly neighborhood developer
+              <span style={{ fontWeight: "bold" }}>
+                "Studies show that keeping your discretionary spending under 30%
+                of your income can significantly boost your savings over time."
+              </span>{" "}
+              -Your friendly neighborhood developer
             </p>
           </div>
         </div>
         <div css={styles.appContainer}>
           <div css={styles.barChartContainer}>
-            <p css={styles.pBottom}>
-              Days until debt free:
-            </p>
+            <p css={styles.pBottom}>Days until debt free:</p>
             <BarChart />
           </div>
         </div>
